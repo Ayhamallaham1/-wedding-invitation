2110193c3b24ba79e53e39ba1acdc2904f5e0da7
Ayhamallaham1 <148724328+Ayhamallaham1@users.noreply.github.com>
2026-07-30 03:19:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function enterWedding(){
+function openWedding(){
 
-document.querySelector(".opening").style.display="none";
+document.getElementById("cover").style.display="none";
 
 document.getElementById("wedding").style.display="block";
 
@@ -9,13 +9,19 @@ top:0,
 behavior:"smooth"
 });
 
+let music = document.getElementById("music");
+
+music.play().catch(()=>{});
+
 }
+
 
 
 
 function toggleMusic(){
 
 let music = document.getElementById("music");
+
 
 if(music.paused){
 
@@ -31,11 +37,12 @@ music.pause();
 
 
 
+
 let weddingDate = new Date("August 21, 2026 19:00:00").getTime();
 
 
 
-let countdown = setInterval(function(){
+setInterval(function(){
 
 
 let now = new Date().getTime();
@@ -44,23 +51,29 @@ let distance = weddingDate - now;
 
 
 
-let days = Math.floor(distance / (1000*60*60*24));
-
-let hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
-
-let minutes = Math.floor((distance % (1000*60*60))/(1000*60));
-
-let seconds = Math.floor((distance % (1000*60))/1000);
+if(distance < 0){
+return;
+}
 
 
 
-document.getElementById("days").innerHTML = days;
+document.getElementById("days").innerHTML =
+Math.floor(distance/(1000*60*60*24));
 
-document.getElementById("hours").innerHTML = hours;
 
-document.getElementById("minutes").innerHTML = minutes;
 
-document.getElementById("seconds").innerHTML = seconds;
+document.getElementById("hours").innerHTML =
+Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+
+
+
+document.getElementById("minutes").innerHTML =
+Math.floor((distance%(1000*60*60))/(1000*60));
+
+
+
+document.getElementById("seconds").innerHTML =
+Math.floor((distance%(1000*60))/1000);
 
 
 
